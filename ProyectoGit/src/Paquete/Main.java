e9158f283cc072f94fba2314279d58e054e756b3
jesuscanebavb <jesusangelcanebaalonsovb@gmail.com>
2021-04-16 12:23:09 +0200

    Subida 1 en rama Prueba

--- ProyectoGit/src/Paquete/Main.java
+++ ProyectoGit/src/Paquete/Main.java
@@ -4,12 +4,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		/*
-		 * Crear el repositorio en la vista Git
+		 * Crear el repositorio local en la vista Git
 		 * Click Der > Team... > Share > Seleccionar el repositorio creado
 		 * Window > References > Team > Git > Label Decoration (Representacion de los comandos Git, componentes, estado de los archivos...)
 		 * */
+		/*
+		 * Conexion a repositorio remoto
+		 * Click Der > Team... > Remote > Push > URI Host y Repository pack automatico si se copia antes el enlace al repositorio >
+		 * Add Specification > Master [Branch] > + Add specification > Finish > Credenciales
+		 * */
+		/*
+		 * Nueva rama
+		 * Vista Git > Click Der Branches > Switch to... > New Branch > NombreRama
+		 * Cambiar de rama (Chackout): Doble click Branches > Local > NombreRama
+		 * 
+		 * */
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo 2");
+		System.out.println("Hola mundo rama Prueba");
 	}
 
 }
