8475d2d55d1787a7817d940afa4bf38deb559b92
jesuscanebavb <jesusangelcanebaalonsovb@gmail.com>
2021-04-16 12:03:05 +0200

    Creacion de la clase Main

---
+++ ProyectoGit/src/Paquete/Main.java
@@ -0,0 +1,15 @@
+package Paquete;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Crear el repositorio en la vista Git
+		 * Click Der > Team... > Share > Seleccionar el repositorio creado
+		 * Window > References > Team > Git > Label Decoration (Representacion de los comandos Git, componentes, estado de los archivos...)
+		 * */
+		System.out.println("Hola mundo");
+		
+	}
+
+}
