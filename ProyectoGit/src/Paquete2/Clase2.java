ed6529bc4dc2fdc2fb69b065b7ad69a97c586991
jesuscanebavb <jesusangelcanebaalonsovb@gmail.com>
2021-04-21 14:10:58 +0200

    Cambio en repoGitHub1 para merge con error

--- ProyectoGit/src/Paquete2/Clase2.java
+++ ProyectoGit/src/Paquete2/Clase2.java
@@ -7,7 +7,7 @@ public class Clase2 {
 		/*Nueva clase en un nuevo paquete*/
 		System.out.println("Hola en Clase2 de Paquete2");
 		System.out.println("Hola 2 en Clase2 de Paquete2");
-		
+		System.out.println("Cambio en Clase2 en repoGitHub1");
 	}
 
 }
